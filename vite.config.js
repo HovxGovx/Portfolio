62d8475f7636b35d44cec72ade98e246f2ca2bf0
HovxGovx <angelotrandria69@gmail.com>
2025-11-20 10:49:45 +0300

    refactor: remove unused import and add alias for src directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import "path";
 import path from 'path';
 
 // https://vite.dev/config/
@@ -9,6 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, './src'),
     },
   },
